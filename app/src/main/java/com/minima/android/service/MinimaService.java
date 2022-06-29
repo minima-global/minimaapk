c29f715a994e5534002d4a7c3066861b743306e6
spartacusrex <patrickcerri@gmail.com>
2022-06-29 17:10:26 +0100

    new help

--- app/src/main/java/com/minima/android/service/MinimaService.java
+++ app/src/main/java/com/minima/android/service/MinimaService.java
@@ -50,7 +50,7 @@ import com.minima.android.MainActivity;
 public class MinimaService extends Service {
 
     static boolean TEST     = true;
-    static boolean GENESIS  = true;
+    static boolean GENESIS  = false;
 
     //Currently Binding doesn't work as we run in a separate process..
     public class MyBinder extends Binder {
