88318800489b5211880b764c58fdafc0b0208cf1
spartacusrex <patrickcerri@gmail.com>
2023-01-03 09:16:42 +0000

    make screenshots not happen

--- app/src/main/java/com/minima/android/MinimaApplication.java
+++ app/src/main/java/com/minima/android/MinimaApplication.java
@@ -1,6 +1,9 @@
 package com.minima.android;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.minima.android.dependencies.backupSync.recurring.BackupRecurringAlarmManager;
 
@@ -9,5 +12,30 @@ public class MinimaApplication extends Application {
     public void onCreate() {
         super.onCreate();
         BackupRecurringAlarmManager.setAlarm(this);
+        setupActivityListener();
+    }
+
+    private void setupActivityListener() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            //Make all activities NOT allow screenshots
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {}
+            @Override
+            public void onActivityResumed(Activity activity) {}
+            @Override
+            public void onActivityPaused(Activity activity) {}
+            @Override
+            public void onActivityStopped(Activity activity) {}
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+            @Override
+            public void onActivityDestroyed(Activity activity) {}
+        });
     }
 }
