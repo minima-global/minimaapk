168652636ff44af2c70206ec6c248cc3c8a00282
spartacusrex <patrickcerri@gmail.com>
2023-01-31 13:37:50 +0000

    latest builds

--- app/src/main/java/com/minima/android/MainActivity.java
+++ app/src/main/java/com/minima/android/MainActivity.java
@@ -512,7 +512,7 @@ public class MainActivity extends AppCompatActivity  implements ServiceConnectio
             new InstallAssetMiniDAPP("news-2.0.mds.zip", MainActivity.this).run();
             new InstallAssetMiniDAPP("scriptide-2.0.mds.zip", MainActivity.this).run();
             new InstallAssetMiniDAPP("terminal-2.03.mds.zip", MainActivity.this).run();
-            new InstallAssetMiniDAPP("wallet_2.3.2.mds.zip", MainActivity.this).run();
+            new InstallAssetMiniDAPP("wallet_2.4.0.mds.zip", MainActivity.this).run();
 
             //And that's that
             SharedPreferences.Editor edit = pref.edit();
